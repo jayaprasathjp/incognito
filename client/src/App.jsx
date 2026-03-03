a764751eaeb925a7705620718dee8faf9fa89775
jayaprasathjp <jayaprasathjp44@gmail.com>
2026-03-04 00:11:35 +0530

    added vercel analytics and speedInsights

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,12 +28,16 @@ import Disputes from './pages/admin/Disputes';
 import Payments from './pages/admin/Payments';
 import Announcements from './pages/admin/Announcements';
 
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Toaster } from 'react-hot-toast';
 import Footer from './components/Footer';
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
+        <Analytics />
+        <SpeedInsights />
         <div className="flex flex-col min-h-screen">
           <Toaster position="top-right" toastOptions={{
             style: {
