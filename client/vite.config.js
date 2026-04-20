fc741c9808b6da815cbec2104a3017bbafc16cda
Jayaprasath K <jayaprasathk@ups.com>
2026-04-21 00:23:24 +0530

    seo prerender

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import prerender from 'vite-plugin-prerender'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    prerender({
+      staticDir: 'dist',
+      routes: ['/', '/leaderboard', '/roadmap', '/rules'],
+    }),
+  ],
 })
