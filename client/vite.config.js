f418dcc79e39737148303cda1923a9222b0c3c80
jayaprasathjp <jayaprasathjp44@gmail.com>
2026-04-21 01:23:07 +0530

    seo fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { createRequire } from 'module'
-import path from 'path'
-const require = createRequire(import.meta.url)
-const prerender = require('vite-plugin-prerender')
-const JSDOMRenderer = require('@prerenderer/renderer-jsdom')
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    prerender({
-      staticDir: path.join(process.cwd(), 'dist'),
-      routes: ['/', '/leaderboard', '/roadmap', '/rules'],
-      renderer: new JSDOMRenderer({
-        renderAfterTime: 1000,
-      }),
-    }),
   ],
 })
